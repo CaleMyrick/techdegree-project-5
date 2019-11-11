cc39f5eb60ae17ba8975bdd4dbd3803350b07ed2
Cale Myrick <cale.myrick@gmail.com>
2019-11-10 23:05:25 -0500

    Final Touches
    Added image styling, finished function for filtering images by captions.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,18 @@
 $("form").on("keyup", $("#search"), function(){
 
-    // value of text input in lowercase
     let $search = $("#search").val().toLowerCase();
 
     for( let i = 0; i < $(".images").length; i++){
 
-        // value of "data-title attribute" field in lowercase
         let $caption = $("a").eq(i).attr("data-caption").toLowerCase();
 
         if(($caption).indexOf($search) === -1){
-            $("img.images").eq(i)
-                        .parent()
-                        .hide();
+            $("img.images").eq(i).parent().hide();
+
+
         } else {
-            $("img.images").eq(i)
-                        .parent()
-                        .show();
+            $("img.images").eq(i).parent().show();
+
         }
 
     }
